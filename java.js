0e385247d7ab5d85951665763ee50ee90f43fcb4
VinayReddy212 <saivinay_arrabolu@tecnics.com>
2023-12-08 18:38:43 +0530

    updated java

--- java.js
+++ java.js
@@ -4,8 +4,13 @@
 Why Java is a platform-independent language 
 1.When we compile java file it will give a byte code(.class file)
 2.With the byte code we can interpret with JVM and then it will convert to machine redable code
-3. javac filename.java -------------> filename.class ----------> machile code
-                        compiles                       JVM
+3. javac filename.java----------------> filename.class -------------------> machile code
+                          Compiles                      JRE(Interpreter)
+                        JDK compiler                           +
+                                                        JIT (Compiler)
+            
+4.When the bytecode is compiled with the JDK it will again interpreted by the JVM and checks the code by line
+by line and JIT compiler will again fastens the process by compiling  and convers to machine code                     
 
              ---------------- JDK vs JRE vs JVM -------------------------
 All JDK JRE and JVM are platform dependent(For every platform there is separate sofwares)
@@ -150,7 +155,7 @@ method ---> cannot be overriden after extension of the class
 class ---> cannot be extended 
 Finally(block) --->Finally block is executed with the try and catch block , This finally block will be executed for sure
 Finalize(method) ---> Finalize method is called when jvm called the garbage collector and it will clean the things up
-with the help of finalize 
+with the help of finalize , Finalize method is present in the object class
 
 class test {
     public static void main(String[] args) {
@@ -171,7 +176,7 @@ default ----> Members declared as default it can be accessed with in the package
 protected ---> Members declared as protected can be accessed with in the package by child classes of outside package
 Public ----> Members declared as public it can be accessed from anywhere from any other class also
 
-----------  Constructors ---------------------------------
+-----------------  Constructors ---------------------------------
 1.No arg constructor
 2.Parameterized constructor
 3.Default constructor
@@ -186,6 +191,23 @@ The class which can create only one object is called as a Singleton class,
 1.Where the constructor should be private so the object cannot be created outside of the class 
 2.getInstance method should be present in the class to create one object and getInstance method should be static
 so we can call the method without creating object
+
+class Animal {
+	
+	public static Animal get_instance = null;
+	
+	private Animal() { // with constructor makes as private we cannot create a object outside the class
+		
+	}
+	
+	public static Animal getInstance() {
+		if(get_instance == null) {
+			return new Animal();
+		}
+		
+		return get_instance;
+	}
+}
 
 ------------- String vs StringBuffer vs StringBuilder --------------------------------
 
@@ -207,6 +229,23 @@ final and abstract is illegal combination for the class
 collection is a root interface for the collection framework and represents a group of objects as a single entity and
 collections is a utility class in java.util which can use this utility methods like(Searching , Sorting)
 for the collection class
+
+                            collection(I)
+                                 |
+                                 |
+               -------------------------------------------
+               |                  |                       |
+               List(I)          Queue(I)               Set(I)     Map(I)
+
+List : (java.util.List)
+1.It will allow duplicate values to store
+2.Access the items easily with indexing
+3.Contains ordered elements
+4.Elements can easily inserted with respect to the position
+
+Set : (java.util.Set)
+1.It will not allow duplicates values
+2.Set don't have ordered elements, So searching and inserting is not done by index based
 
 ---------------- Array vs ArrayList ----------------------------
 Array is a primitive datatype in java and ArrayList is a collection class 
@@ -233,7 +272,61 @@ of linkedlist
 elements in arraylist.
 8.Insertion and deletion operation is Expensive in array whereas in linkedlist deletion and insertion is easy
 
+----------------- Marker Interface -----------------------
+
+Marker interface is a empty interface which dont have any member variables and member methods, Marker interfaces
+are present to just information that it is related to cloning or serialization
+Ex : Cloneable , Serializable
 
 
+---------------- Custom immutable class in java ---------------
+1.Class should be final so it cannot be extended
+2.Members should be private(scope is limmited) and final so once it is assigned it cannot be changed
+3.varibles should be initialized in constructor so it cannot be reassigned since it is a final varibles
+4.Avoid setter methods in this Custom immutable class
+5.If calss has mutable objects then only return copy of objects so that it will be immutable
+
+final class ImmutableClass {
+		
+	private final int number;
+	private final String str;
+	private final List immutableList;
+	
+	public ImmutableClass(int number , String str,List immutableList) {
+			this.number = number;
+			this.str = str;			
+			if (immutableList != null) {
+	            this.immutableList = new ArrayList<>(immutableList);
+	        } else {
+	            this.immutableList = new ArrayList<>();
+	        }
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public String getStr() {
+		return "peter";
+	}
+	
+	public List getImmutableList() {
+		return new ArrayList<>(immutableList);
+	}
+	
+	
+	
+}
+
+----------------- Default methods in Object class ----------------
+1.clone method
+2.equals method
+3.finalize method
+4.toString 
+5.hashcode
+6.getClass
+7.notify
+8.notifyAll 
+9.wait
 
 */
